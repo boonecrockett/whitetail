e7379a2fc2a8a6cb93ff62397205c117206ffc11
boonecrockett <97560768+boonecrockett@users.noreply.github.com>
2024-07-12 13:03:13 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const { useState } = React;
-
 const Alert = ({ children, className }) => (
     <div className={`bg-yellow-100 border-l-4 border-yellow-500 text-yellow-700 p-4 ${className}`} role="alert">
         {children}
